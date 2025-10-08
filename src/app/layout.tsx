e288ea2006f797dfb891fa23accdf94bd408944f
Olumide <olumide.m.ojo@gmail.com>
2025-10-08 14:29:22 +0100

    fix: SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Africima : Creating and Reshaping a Better Africa",
+  title: "Africima | Creating and Reshaping a Better Africa",
   description:
     "Africima is a business development firm leveraging creativity and innovation to deliver sustainable, efficient solutions across sectors in Africa.",
 };
